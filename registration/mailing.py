9578c7c426b9ef227aebbff635ba40ff8f9c86f8
Emiel van de Laar <gemiel@gmail.com>
2016-12-01 21:00:02 +0100

    Fixes reverse in mailing

--- registration/mailing.py
+++ registration/mailing.py
@@ -7,9 +7,9 @@ from django.template.loader import render_to_string
 def send_thanks_mail(registration):
     subject = "[SF2017] Signup received"
 
-    context = {'registration': registration, 'subject': subject}
-    text_msg = render_to_string('mail/thanks.text', context=context)
-    html_msg = render_to_string('mail/thanks.html', context=context)
+    context = {"registration": registration, "subject": subject}
+    text_msg = render_to_string("mail/thanks.text", context=context)
+    html_msg = render_to_string("mail/thanks.html", context=context)
 
     send_mail(subject, text_msg, settings.DEFAULT_FROM_EMAIL,
             [registration.email], fail_silently=False,
@@ -17,13 +17,13 @@ def send_thanks_mail(registration):
 
 
 def send_completion_mail(registration):
-    path = reverse('payment', args=[registration.id])
+    path = reverse("status", args=[registration.id])
     url = "{}{}".format(settings.EMAIL_BASE_URI, path)
     subject = "[SF2017] Complete registration"
 
-    context = {'registration': registration, 'subject': subject, 'url': url}
-    text_msg = render_to_string('mail/complete.text', context=context)
-    html_msg = render_to_string('mail/complete.html', context=context)
+    context = {"registration": registration, "subject": subject, "url": url}
+    text_msg = render_to_string("mail/complete.text", context=context)
+    html_msg = render_to_string("mail/complete.html", context=context)
 
     send_mail(subject, text_msg, settings.DEFAULT_FROM_EMAIL,
            [registration.email], fail_silently=False,
@@ -32,10 +32,10 @@ def send_completion_mail(registration):
 
 def send_payment_mail(registration):
     subject = "[SF2017] Payment received"
-    context = {'registration': registration, 'subject': subject}
+    context = {"registration": registration, "subject": subject}
 
-    text_msg = render_to_string('mail/payment.text', context=context)
-    html_msg = render_to_string('mail/payment.html', context=context)
+    text_msg = render_to_string("mail/payment.text", context=context)
+    html_msg = render_to_string("mail/payment.html", context=context)
 
     send_mail(subject, text_msg, settings.DEFAULT_FROM_EMAIL,
             [registration.email], fail_silently=False,
@@ -43,13 +43,13 @@ def send_payment_mail(registration):
 
 
 def send_payment_reminder_mail(registration):
-    path = reverse('complete', args=[registration.id])
+    path = reverse("status", args=[registration.id])
     url = "{}{}".format(settings.EMAIL_BASE_URI, path)
     subject = "[SF2017] Payment reminder"
 
-    context = {'registration': registration, 'subject': subject, 'url': url}
-    text_msg = render_to_string('mail/payment_reminder.text', context=context)
-    html_msg = render_to_string('mail/payment_reminder.html', context=context)
+    context = {"registration": registration, "subject": subject, "url": url}
+    text_msg = render_to_string("mail/payment_reminder.text", context=context)
+    html_msg = render_to_string("mail/payment_reminder.html", context=context)
 
     send_mail(subject, text_msg, settings.DEFAULT_FROM_EMAIL,
             [registration.email], fail_silently=False,
