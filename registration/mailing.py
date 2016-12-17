058824e2a9e263980e8ddbfec040c301349970b6
Emiel van de Laar <gemiel@gmail.com>
2016-12-17 20:48:19 +0100

    Lets html2text handle text mail

--- registration/mailing.py
+++ registration/mailing.py
@@ -2,14 +2,15 @@ from django.conf import settings
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
+from html2text import html2text
 
 
 def send_thanks_mail(registration):
     subject = "[SF2017] Signup received"
 
     context = {"registration": registration, "subject": subject}
-    text_msg = render_to_string("mail/thanks.text", context=context)
     html_msg = render_to_string("mail/thanks.html", context=context)
+    text_msg = html2text(html_msg)
 
     send_mail(subject, text_msg, settings.DEFAULT_FROM_EMAIL,
               [registration.email], fail_silently=False,
@@ -22,8 +23,8 @@ def send_completion_mail(registration):
     subject = "[SF2017] Complete registration"
 
     context = {"registration": registration, "subject": subject, "url": url}
-    text_msg = render_to_string("mail/complete.text", context=context)
     html_msg = render_to_string("mail/complete.html", context=context)
+    text_msg = html2text(html_msg)
 
     send_mail(subject, text_msg, settings.DEFAULT_FROM_EMAIL,
               [registration.email], fail_silently=False,
@@ -34,8 +35,8 @@ def send_payment_mail(registration):
     subject = "[SF2017] Payment received"
     context = {"registration": registration, "subject": subject}
 
-    text_msg = render_to_string("mail/payment.text", context=context)
     html_msg = render_to_string("mail/payment.html", context=context)
+    text_msg = html2text(html_msg)
 
     send_mail(subject, text_msg, settings.DEFAULT_FROM_EMAIL,
               [registration.email], fail_silently=False,
@@ -48,8 +49,8 @@ def send_payment_reminder_mail(registration):
     subject = "[SF2017] Payment reminder"
 
     context = {"registration": registration, "subject": subject, "url": url}
-    text_msg = render_to_string("mail/payment_reminder.text", context=context)
     html_msg = render_to_string("mail/payment_reminder.html", context=context)
+    text_msg = html2text(html_msg)
 
     send_mail(subject, text_msg, settings.DEFAULT_FROM_EMAIL,
               [registration.email], fail_silently=False,
