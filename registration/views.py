ba045d2e8c15e5c860b539ce270b6a5234ee375b
Emiel van de Laar <gemiel@gmail.com>
2017-01-07 12:58:41 +0100

    Finished perf test

--- registration/views.py
+++ registration/views.py
@@ -17,7 +17,6 @@ from .models import Registration
 logger = logging.getLogger(__name__)
 
 
-@csrf_exempt # TESTING
 @require_http_methods(["GET", "POST"])
 def signup(request):
     if request.method == "POST":
