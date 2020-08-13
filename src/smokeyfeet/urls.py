1dbddd9cffe4f94b97a3d834f02f7cdbbd9bd2ce
Emiel van de Laar <gemiel@gmail.com>
2020-08-13 20:54:30 +0200

    Fix root 404

--- src/smokeyfeet/urls.py
+++ src/smokeyfeet/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import urls as auth_urls
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include(auth_urls)),
-    path("/", include("smokeyfeet.registration.urls", namespace="registration")),
+    path("", include("smokeyfeet.registration.urls", namespace="registration")),
     path("mollie/", include("smokeyfeet.mollie_webhook.urls", namespace="mollie_webhook")),
     path("shop/", include("smokeyfeet.minishop.urls", namespace="minishop")),
 ]
